cc6922cbfe61bc9b8a2d3e921a09f539b445c323
GoustYTC <150684958+GoustYTC@users.noreply.github.com>
2024-05-28 22:05:32 +0700

    1

---
+++ Assets/VolumeSliders.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class VolumeSliders : MonoBehaviour
+{
+    [Serializable]
+    public class VolumeClass
+    {
+        public AudioMixerGroup audioMixerGroup;
+        public Slider slider;
+    }
+    [SerializeField] List<VolumeClass> _volumes;
+
+    public void Start() => SetVolumeToAllMixers();
+
+    public void SetVolumeToAllMixers()
+    {
+        foreach (var item in _volumes)
+        {
+            item.audioMixerGroup.audioMixer.SetFloat(item.audioMixerGroup.name + "Volume", item.slider.value);
+        }
+    }
+}
