76f2ebd393b2a740073602eccf115f74323eafa7
GoustYTC <150684958+GoustYTC@users.noreply.github.com>
2024-05-28 20:52:19 +0700

    1

---
+++ Assets/Damager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out IDamagable _damagable))
+            _damagable.ApplyDamage(1);
+    }
+}
