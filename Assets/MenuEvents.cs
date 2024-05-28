257c2132555f728bb1953b5d381c6fc93531aca6
GoustYTC <150684958+GoustYTC@users.noreply.github.com>
2024-05-28 21:27:40 +0700

    1

---
+++ Assets/MenuEvents.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuEvents : MonoBehaviour
+{
+    public void OpenScene(string _sceneName)
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+
+    public void OpenURL(string _URL)
+    {
+        Application.OpenURL(_URL);
+    }
+}
