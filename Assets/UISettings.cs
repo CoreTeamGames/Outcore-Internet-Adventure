0ea2968d8ddfe15a33594387053104a97eee0f44
GoustYTC <150684958+GoustYTC@users.noreply.github.com>
2024-05-29 19:51:43 +0700

    Изменения
    Исправлены небольшие ошибки (Добавлен класс UISettings)
    Удалены демо из ассета Kirurobo

---
+++ Assets/UISettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace OutcoreInternetAdventure.UI
+{
+    /// <summary>
+    /// The class Settings of UI
+    /// </summary>
+    [CreateAssetMenu(menuName = "Outcore SDK/UI Settings")]
+    public class UISettings : ScriptableObject
+    {
+        public Color PanelColor { get { return _panelColor; } }
+        public float MenuShowFadingDuration { get { return _menuShowFadingDuration; } }
+
+        [SerializeField] Color _panelColor;
+        [SerializeField] float _menuShowFadingDuration;
+    }
+}
