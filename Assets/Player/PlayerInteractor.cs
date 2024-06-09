cb86adabc612927c0a7e85fa424fd24003459f30
GoustYTC <150684958+GoustYTC@users.noreply.github.com>
2024-06-09 19:14:52 +0700

    Added player interactor hint

--- Assets/Player/PlayerInteractor.cs
+++ Assets/Player/PlayerInteractor.cs
@@ -1,57 +1,46 @@
-﻿using UnityEngine.InputSystem;
+﻿using UnityEngine.Events;
 using UnityEngine;
-using TMPro;
+
 
 namespace OutcoreInternetAdventure.Player
 {
     public class PlayerInteractor : MonoBehaviour
     {
         [HideInInspector] public PlayerEvents events;
-        InputActionMap _inputActions;
         IInteractor _interactor;
-        [SerializeField] GameObject _interactorHint;
-        [SerializeField] TMP_Text _interactorKeyText;
-        [SerializeField] string _interactorKeyName = "Interact";
-        [SerializeField] Vector2 _offset = Vector2.up;
-        Transform _firstRoot;
-        Vector3 _firstPosition;
+        Collider2D _collision;
+        [SerializeField] UnityEvent _onInteractAvaliable;
+        [SerializeField] UnityEvent _onInteractNonAvaliable;
+        [SerializeField] UnityEvent _onInteract;
 
-        public void Awake()
-        {
-            _firstPosition = _interactorHint.transform.localPosition;
-            _firstRoot = _interactorHint.transform.parent;
-            _inputActions = gameObject.transform.parent.gameObject.GetComponentInChildren<UnityEngine.InputSystem.PlayerInput>().currentActionMap;
-        }
+        public Collider2D Collision { get { return _collision; } }
 
         public void OnTriggerEnter2D(Collider2D collision)
         {
-            if (collision.TryGetComponent<IInteractor>(out IInteractor interactor))
+            if (collision.GetComponent<IInteractor>() != null)
             {
-                _interactor = interactor;
-                InitializeHint();
-                _interactorHint.transform.parent = collision.transform;
-                _interactorHint.transform.localPosition = new Vector3(_offset.x,_offset.y,_firstPosition.z);
-                _interactorHint.SetActive(true);
+            _interactor = collision.GetComponent<IInteractor>();
+            _collision = collision;
+            _onInteractAvaliable?.Invoke();
             }
         }
 
-        void InitializeHint()
-        {
-            _interactorKeyText.text = _inputActions.FindAction(_interactorKeyName).bindings[0].path.Split('/')[1];
-        }
+
 
         public void TryInteract()
         {
             if (_interactor != null)
+            {
                 _interactor.StartEvent();
+                _onInteract?.Invoke();
+            }
         }
 
         public void OnTriggerExit2D(Collider2D collision)
         {
             _interactor = null;
-            _interactorHint.SetActive(false);
-            _interactorHint.transform.parent = _firstRoot;
-            _interactorHint.transform.localPosition = _firstPosition;
+            _onInteractNonAvaliable?.Invoke();
+            _collision = null;
         }
     }
 }
